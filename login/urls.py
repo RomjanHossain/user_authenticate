e8dc7b370b90b91e817494393c11856e3e8f5f55
RomjanHossain <romjanhossain726526@gmail.com>
2020-01-12 18:00:39 +0600

    a

--- login/urls.py
+++ login/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('sign_up/', views.sign_up, name='signup'),
+    path('index/', views.index, name='index'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 
+def index(request):
+    return render(request, 'login/index.html')
+
+
 def home(request):
-    return render(request, 'login/home.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('home')
+    else:
+        return render(request, 'login/home.html')
 
 
 def sign_up(request):
